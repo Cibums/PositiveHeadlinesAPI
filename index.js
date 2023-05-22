1dccaab69e2f6b4421aaf455b6640afc4a419d75
Cibums <lucas.fransson@hotmail.com>
2023-05-22 19:26:29 +0200

    prompt from database

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(cors({origin: true}));
 app.use(express.json());
 
+var prompt = "Here's a news article. I don't know what language this is in, but give me a title of this article. The title should be in the language of the article and you should keep it short and informative. It should not be clickbait. The title should accurately describe the content of the article and you should try to make it as positive as possible. If the article is not a positive one, then try to make the title as positive as possible. But remember that the title should be accurate and truthful to the article's contents. That's the most important part. The title you give me should always be true, based on the articles's content. It's also very important that your response is only the title itself. You should only respond with the title. Nothing else than the title should be included in your response. Do not include translations, or citation marks. Do not include anything in your response that is not the title itself. Here's the article: ";
+
 var serviceAccount = require("./permissions.json");
 
 admin.initializeApp({
@@ -17,6 +19,19 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+
+try{
+    const document = db.collection("system").doc("variables");
+    let item = await document.get();
+
+    if(!item.exists){
+        let response = item.data();
+
+        prompt = response.prompt;
+    }
+}catch(error){
+    console.log(error);
+}
 
 app.post('/title', (req,res) => {
     (async ()=> {
@@ -33,9 +48,9 @@ app.post('/title', (req,res) => {
             if(!item.exists){
                 if(await addTitle(req.body.domain, req.body.title, req.body.articleText)){
                     const document = db.collection(enc_domain).doc(enc_title);
-                    let item = await document.get();
-                    let response = item.data();
-                    return res.status(201).send(response);
+                    let item1 = await document.get();
+                    let response1 = item1.data();
+                    return res.status(201).send(response1);
                 }
                 else{
                     return res.status(500).send(error);
@@ -59,7 +74,7 @@ async function getGPTTitle(articleText){
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{"role": "user", "content": "Here's a news article. I don't know what language this is in, but give me a title of this article. The title should be in the language of the article and you should keep it short and informative. It should not be clickbait. The title should accurately describe the content of the article and you should try to make it as positive as possible. If the article is not a positive one, then try to make the title as positive as possible. But remember that the title should be accurate and truthful to the article's contents. That's the most important part. The title you give me should always be true, based on the articles's content. It's also very important that your response is only the title itself. You should only respond with the title. Nothing else than the title should be included in your response. Do not include translations, or citation marks. Do not include anything in your response that is not the title itself. Here's the article: " + articleText}]
+        messages: [{"role": "user", "content": prompt + articleText}]
     });
 
     return completion.data.choices[0].message.content;
