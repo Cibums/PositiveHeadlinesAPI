ffbb7ab002a787b358d3a8ed331bcbf8fcb0be4a
Cibums <lucas.fransson@hotmail.com>
2023-05-22 19:41:35 +0200

    fixes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(cors({origin: true}));
 app.use(express.json());
 
 var prompt = "Here's a news article. I don't know what language this is in, but give me a title of this article. The title should be in the language of the article and you should keep it short and informative. It should not be clickbait. The title should accurately describe the content of the article and you should try to make it as positive as possible. If the article is not a positive one, then try to make the title as positive as possible. But remember that the title should be accurate and truthful to the article's contents. That's the most important part. The title you give me should always be true, based on the articles's content. It's also very important that your response is only the title itself. You should only respond with the title. Nothing else than the title should be included in your response. Do not include translations, or citation marks. Do not include anything in your response that is not the title itself. Here's the article: ";
+var promptGottenOnce = false;
 
 var serviceAccount = require("./permissions.json");
 
@@ -20,14 +21,17 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-try {
-    getDocument();
-} catch (error) {
-    console.log(error);
-}
-
 app.post('/title', (req,res) => {
     (async ()=> {
+
+        if(promptGottenOnce === false){
+            try {
+                getDocument();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
         var utf8Domain = encodeURIComponent(req.body.domain);
         var enc_domain = btoa(utf8Domain);
         var utf8Title = encodeURIComponent(req.body.title);
@@ -98,9 +102,10 @@ async function getDocument() {
     const document = db.collection("system").doc("variables");
     let item = await document.get();
 
-    if (!item.exists) {
+    if (item.exists) {
         let response = item.data();
         prompt = response.prompt;
+        promptGottenOnce = true;
     }
 }
 
