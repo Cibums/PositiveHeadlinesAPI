fd6fe33b0b48a49306c8a1b398a6ac2de4a353e0
Cibums <lucas.fransson@hotmail.com>
2023-05-22 16:12:21 +0200

    added chatgpt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const admin = require("firebase-admin");
 const express = require('express');
 const cors = require('cors');
+const { Configuration, OpenAIApi } = require("openai");
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -16,12 +17,12 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-app.get('/get/:domain/:title', (req,res) => {
+app.post('/title', (req,res) => {
     (async ()=> {
 
-        var utf8Domain = encodeURIComponent(req.params.domain);
+        var utf8Domain = encodeURIComponent(req.body.domain);
         var enc_domain = btoa(utf8Domain);
-        var utf8Title = encodeURIComponent(req.params.title);
+        var utf8Title = encodeURIComponent(req.body.title);
         var enc_title = btoa(utf8Title);
 
         try{
@@ -30,14 +31,14 @@ app.get('/get/:domain/:title', (req,res) => {
             let response = item.data();
 
             if(!item.exists){
-                if(await addTitle(req.params.domain, req.params.title)){
+                if(await addTitle(req.body.domain, req.body.title, req.body.articleText)){
                     const document = db.collection(enc_domain).doc(enc_title);
                     let item = await document.get();
                     let response = item.data();
                     return res.status(201).send(response);
                 }
                 else{
-                    
+                    return res.status(500).send(error);
                 }
             }
 
@@ -50,13 +51,23 @@ app.get('/get/:domain/:title', (req,res) => {
     })();
 });
 
-async function getGPTTitle(title){
-    return "ny titel";
+async function getGPTTitle(articleText){
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY || "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",
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "user", "content": "Here's a news article. I don't know what language this is in, but give me a title of this article. The title should be in the language of the article and you should keep it short and informative. It should not be clickbait. The title should accurately describe the content of the article and you should try to make it as positive as possible. If the article is not a positive one, then try to make the title as positive as possible. But remember that the title should be accurate and truthful to the article's contents. That's the most important part. The title you give me should always be true, based on the articles's content. It's also very important that your response is only the title itself. You should only respond with the title. Nothing else than the title should be included in your response. Here's the article: " + articleText}]
+    });
+
+    return completion.data.choices[0].message;
 }
 
-async function addTitle(domain, title){
+async function addTitle(domain, title, articleText){
     try {
-        var nt = await getGPTTitle(title);
+        var nt = await getGPTTitle(articleText);
 
         var utf8Domain = encodeURIComponent(domain);
         var enc_domain = btoa(utf8Domain);
