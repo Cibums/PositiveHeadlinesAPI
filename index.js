61f154b4fb02bdb9fb637898e2efadbf588e185e
Cibums <lucas.fransson@hotmail.com>
2023-05-24 00:09:08 +0200

    Moved permissions to env

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 
 const prompt = "Give me a headline to this article. Your response should be only the headline and nothing else. Here's the article: ";
 
-const serviceAccount = require("./permissions.json");
+let serviceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
